a3ae37b26201e13c233349091e5dfef337516d22
ASHWINI <71061555+ashwiniv66@users.noreply.github.com>
2026-05-19 20:04:38 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for(int x : nums ){
+            if(seen.contains(x)){
+                return true;
+            }seen.add(x);
+        }
+        return false;
+    }
+}
